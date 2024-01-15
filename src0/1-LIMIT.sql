287f69f427af9c76bbcf2f1353eef541166f102c
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2024-01-15 14:47:32 +0700

    belajar mysql: query (sqlite)

---
+++ src0/1-LIMIT.sql
@@ -0,0 +1,5 @@
+-- Limit results to first 3
+SELECT "title", "author" FROM "longlist" LIMIT 3;
+
+-- Limit results to first 10
+SELECT "title", "author" FROM "longlist" LIMIT 5,5; -- skip 5 awal dan lajut 5 setelahnya
